c1deeb618b3853248add1c881f1427f7a449c5bb
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-08-29 09:41:07 -0300

    add mint page

---
+++ pages/mint.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+import React from 'react'
+import { NFTLayout } from '../components/nft/NFTLayout'
+
+const mint:NextPage = () => {
+  return (
+    <div>
+      <NFTLayout />
+    </div>
+  )
+}
+
+export default mint
