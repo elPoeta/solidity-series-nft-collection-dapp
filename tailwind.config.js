267c68d88c0712442c37d9aaa5dc6828a3c5413a
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-08-31 09:18:49 -0300

    add logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      rotate: {
+        15: "15deg",
+      },
+    },
   },
   plugins: [],
 };
